494474097f5c21de75a66867f0932969bc759105
Mikhail Osipov <mishaend@gmal.com>
2026-01-17 19:54:23 +0100

    3-calc done

---
+++ 3-calc-events/index.js
@@ -0,0 +1,43 @@
+import EventEmitter from "node:events";
+const em = new EventEmitter()
+
+let firstNum = process.argv[2];
+let secondNum = process.argv[3];
+let opr = process.argv[4];
+
+
+
+em.on('result', (res)=> console.log(res));
+em.on('add', (a,b) => {
+  em.emit('result', a+b);
+});
+
+em.on('subtract', (a,b) => {
+  em.emit('result', a-b);
+});
+
+em.on('division', (a,b) => {
+  em.emit('result', a/b);
+});
+
+em.on('multiply', (a,b) => {
+  em.emit('result', a*b);
+});
+
+calc(firstNum,secondNum,opr);
+
+function calc(a,b,op){
+    if(!parseInt(a) || !parseInt(b)) {
+      console.log("use numbers in operations.");
+      return;
+    }
+    
+    if(em.eventNames().includes(op)){
+      em.emit(op, Number(a),Number(b));
+    }else{
+      console.log("invalid operation: "+ op);
+      return;
+    }
+
+}
+
---
+++ 3-calc/index.js
@@ -0,0 +1,30 @@
+import path from "path";
+import { fileURLToPath, pathToFileURL } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let firstNum = process.argv[2];
+let secondNum = process.argv[3];
+let opr = process.argv[4];
+
+calc(firstNum, secondNum, opr);
+
+async function calc(num1, num2, operation)
+{
+  try {
+
+    if(!parseInt(num1) || !parseInt(num2)) {
+      console.log("use numbers in operations.");
+      return;
+    }
+    
+    const filePath =  path.join(__dirname, `${operation}.js`)
+    const file = await import(pathToFileURL(filePath).href);
+    
+    console.log(file.calc(parseInt(num1),parseInt(num2)));
+  }catch(err) {
+    console.log("invalid operation: "+ operation);
+    return;
+  } 
+}
