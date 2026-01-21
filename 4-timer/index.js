37282086a905ba3590bb779c81309430b7a1f0f2
Mikhail Osipov <mishaend@gmal.com>
2026-01-21 22:15:58 +0100

    done with timer

---
+++ 4-timer/index.js
@@ -0,0 +1,52 @@
+const input = process.argv;
+
+startProcess(input);
+
+function startProcess(input){
+  duration = 0;
+    let start = 2;
+    let stop = input.length;
+    let message = "Ваш таймер сработает через: "
+    while(start < stop)
+    {
+      console.log(input[start]);
+      message += `${input[start]} `;
+      duration += parseInput(input[start]);
+      start++;
+    }
+
+    console.log(message);
+    const timerID = setTimeout(()=> {
+      console.log("Конец")
+    }, duration);
+}
+
+function parseInput(value)
+{
+  let res = "";
+    if(value.includes('h')){
+    res = splitter(value, 'h')
+      if(!isNaN(Number(res))) {
+        return Number(res) * 60 * 60 * 1000;
+      }
+    }
+
+    if(value.includes('m')){
+        res = splitter(value, 'm')
+          if(!isNaN(Number(res))) {
+            return Number(res) * 60 * 1000;
+      }
+    }
+    if(value.includes('s')){
+        res = splitter(value, 's')
+          if(!isNaN(Number(res))) {
+            return Number(res) * 1000;
+      }
+    }
+
+    return 0;
+}
+
+function splitter(string,char){
+  return string.split('').filter(c => c != char).join();
+}
